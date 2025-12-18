6e22d6bba38fac2f8859232825ba61e382bfa640
Samraysh Pellakur <pellakur.samraysh@gmail.com>
2025-12-18 12:53:12 -0600

    Added client review section to preview page

---
+++ js/plugins.js
@@ -0,0 +1,20 @@
+
+	
+	$(".test-owl").owlCarousel({
+		loop: true,
+		responsiveClass: true,
+		margin: 10,
+		nav: true,
+		dots: false,
+		dotsEach: false,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			}
+		}
+	});
+
+	
